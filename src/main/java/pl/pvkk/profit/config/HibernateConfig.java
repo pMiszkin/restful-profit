fc86c9a5c18b759329f9b847cd7b3ffe420c9607
pavlakk <pavlakk@git.com>
2017-07-14 17:41:47 +0200

    just change 'update' to 'create-drop'

--- src/main/java/pl/pvkk/profit/config/HibernateConfig.java
+++ src/main/java/pl/pvkk/profit/config/HibernateConfig.java
@@ -30,7 +30,7 @@ public class HibernateConfig {
 
 	Properties additionalProperties() {
 	    Properties properties = new Properties();
-	    properties.setProperty("hibernate.hbm2ddl.auto", "update");
+	    properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 	    properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
 	    properties.setProperty("hibernate.show_sql", "true");
 	    properties.setProperty("hibernate.transaction", "true");
